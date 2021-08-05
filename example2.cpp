3b0579640aa90c0a804c5b79d4cd056d8d0c9ebe
manuel <you@example.com>
2021-08-05 15:00:31 +0200

    Add normals to intersection function

--- example2.cpp
+++ example2.cpp
@@ -1,21 +1,24 @@
 #include <intersections.hpp>
 #include <iostream>
 int main(){
-    ray<float> r(vec3f(0, 0, 0),
-                 vec3f(0, 0, 1));
-    aabb<float> b(vec3f(-1, -1, 1), //Construct aabb using
-                  vec3f( 1,  1, 2));//minima and maxima in each dimension
+    ray<float> r(vec3f(5, 0, 0),
+                 vec3f(-1, 0, 0));
+    aabb<float> b(vec3f(-1, -1, -1), //Construct aabb using
+                  vec3f( 1,  1, 1));//minima and maxima in each dimension
 
     
-    std::optional<double_intersection<float>> dints_opt = intersect(r, b);
+    std::optional<std::pair<double_intersection<float>, vec3_tm<float>>> dints_opt = intersect_with_normal(r, b);
 
     //A ray can intersect twice with an aabb. Therefore the result is a double_intersection.
+    bool x;
+    x = dints_opt ? true : false;
     if(dints_opt.has_value()){
-        double_intersection<float> dints = dints_opt.value();
+        double_intersection<float> dints = dints_opt.value().first;
         std::cout << dints.p.first  << " First intersection point\n";
         std::cout << dints.p.second << " Second intersection point\n";
 
         std::cout << dints.t.first  << " First intersection t\n";
         std::cout << dints.t.second << " Second intersection t\n";
+        std::cout << dints_opt.value().second << "\n";
     }
 }
--- include/ray_aabb.hpp
+++ include/ray_aabb.hpp
@@ -29,4 +29,51 @@ intersect(const ray<scalar>& r, const aabb<scalar>& b) {
     ret.v = 0;
     return ret;
 }
+
+template<typename scalar> 
+    requires(is_primitive_scalar<scalar>) 
+std::optional<std::pair<double_intersection<scalar>, vec3_tm<scalar>>> intersect_with_normal(const ray<scalar>& r, const aabb<scalar>& b) {
+    scalar tmin = -INFINITY, tmax = INFINITY;
+    using std::max;
+    using std::min;
+
+    int dim_hit = -1;
+    for (int i = 0; i < 3; ++i) {
+        scalar t1 = (b.start(i) - r.o(i)) * r.id(i);
+        scalar t2 = (b.end(i)   - r.o(i)) * r.id(i);
+        auto tminold = tmin;
+        tmin = max(tmin, min(t1, t2));
+        if constexpr(is_primitive_scalar<scalar>){
+            if(tmin != tminold){
+                dim_hit = i;
+            }
+        }
+        tmax = min(tmax, max(t1, t2));
+    }
+    double_intersection<scalar> ret;
+    if constexpr(is_primitive_scalar<scalar>)
+        if(tmax < tmin){
+            return std::nullopt;
+        }
+    if constexpr(!is_primitive_scalar<scalar>)
+        ret.hitmask = tmax >= tmin;
+    
+    ret.t.first = tmin;
+    ret.t.second = tmax;
+    ret.p.first = r.o + r.d * tmin;
+    ret.p.second = r.o + r.d * tmax;
+    ret.u = 0;
+    ret.v = 0;
+    vec3_tm<scalar> normal;
+    if(dim_hit == 0){
+        normal = vec3_tm<scalar>(1, 0, 0) * (std::signbit(r.d.x()) ? 1 : -1);
+    }
+    if(dim_hit == 1){
+        normal = vec3_tm<scalar>(0, 1, 0) * (std::signbit(r.d.x()) ? 1 : -1);
+    }
+    if(dim_hit == 2){
+        normal = vec3_tm<scalar>(0, 0, 1) * (std::signbit(r.d.x()) ? 1 : -1);
+    }
+    return std::make_pair(ret, normal);
+}
 #endif
